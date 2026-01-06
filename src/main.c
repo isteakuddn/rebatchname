831dcd9ee0d41a1ba1be175b83c4454887494dc4
isteak <isteakuddn@gmail.com>
2026-01-07 00:49:11 +0600

    src/main.c update

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
-    printf("Hello, world!\n");
+int main(void)
+{
+    int dogs;
+
+    printf("How many dogs do you have?\n");
+    scanf("%d", &dogs);
+    printf("So you have %d dog(s)!\n", dogs);
+
     return 0;
 }
